eade72c155e99c0a282a227e3e0b46f217386965
Brandon Nelson <machinecar998@gmail.com>
2022-11-03 08:33:31 -0500

    Added menu and rewrote code to fit inside choices

--- Program.cs
+++ Program.cs
@@ -13,33 +13,41 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            try
-            {
-
                 // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
+                Console.WriteLine("1.) Display all blogs");
+                Console.WriteLine("2.) Add blog");
+                Console.WriteLine("3.) Create post");
+                Console.WriteLine("4.) Display posts");
+                string input = Console.ReadLine();
 
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.Name);
+                if(input =="1"){
+                    try{
+                        var db = new BloggingContext();
+                        var query = db.Blogs.OrderBy(b => b.Name);
+                        Console.WriteLine("All blogs in the database");
+                        foreach (var item in query){
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    catch(Exception ex){
+                        logger.Error(ex.Message);
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
-
+                if (input == "2"){
+                    try{
+                        Console.WriteLine("Enter a name for a new Blog: ");
+                        String blogName = Console.ReadLine();
+                        var blog = new Blog { Name = blogName };
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {blogName}", blogName);
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+                    }
+                    catch (Exception ex){
+                        logger.Error(ex.Message);
+                        }
+                }
             logger.Info("Program ended");
         }
     }
